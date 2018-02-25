3eaf94f360056c35e61cf71ed3d6a6d4af02ab86
Shao-Chun Ma <justwhenithought@gmail.com>
2018-02-25 17:01:32 +0100

    beautify job titles

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -13,7 +13,13 @@ class IndexController extends Controller
     public function index()
     {
         $jobs = $this->getDoctrine()->getRepository(Job::class)->findAll();
-
+        foreach ($jobs as &$job) {
+            $job->setTitle(ucwords(strtolower($job->getTitle())));
+            $job->setTitle(str_replace("Php", "PHP", $job->getTitle()));
+            $job->setTitle(str_replace(".net", ".NET", $job->getTitle()));
+            $job->setTitle(str_replace("java", "Java", $job->getTitle()));
+            $job->setTitle(str_replace("symfony", "Symfony", $job->getTitle()));
+        }
         return $this->render('pages/index.html.twig', array(
             'jobs' => $jobs,
         ));
